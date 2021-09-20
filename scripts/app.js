46ebede32871bd9b43156ebbffbfce170655f772
tonyv <tonyvilla77@gmail.com>
2021-09-20 14:01:43 -0700

    added film frames on screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,17 @@
 // Variables
+// Display Elements
+const $controllerEl = $('.game-controls');
+const $filmWrapEl = $('.film-wrapper');
 
+// Buttons
 const $playBtn = $('#play-button');
 const $lightsBtn = $('.lights');
 const $rollCamBtn = $('.roll-cam');
 const $reloadMagBtn = $('.reload-mag');
+
+$playBtn.on('click', () => {
+  $playBtn.fadeOut(1000);
+  $controllerEl.removeClass('hidden');
+  //   $controllerEl.fadeIn(1500);
+  //   $filmWrapEl.append('<div class="film-frame"></div>');
+});
