67c1b99710949751f278f4872712a6a61933296c
tonyv <tonyvilla77@gmail.com>
2021-09-20 16:28:34 -0700

    magDec and magReload now display dynamically

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,9 @@ const $filmWrapEl = $('.film-wrapper');
 const $filmCan1El = $('.film-can-1');
 const $filmCan2El = $('.film-can-2');
 
+// Score Elements
+const $filmRemaning = $('.film-remain-actual');
+
 // Buttons
 const $playBtn = $('#play-button');
 const $lightsBtn = $('.lights');
@@ -42,12 +45,16 @@ const frameDecrement = () => {
 $playBtn.on('click', () => {
   $playBtn.fadeOut(1000);
   $controllerEl.removeClass('hidden');
+  $filmRemaning.html(filmRemain.currentFrame);
   //   $controllerEl.fadeIn(1500);
-  //   $filmWrapEl.append('<div class="film-frame"></div>');
+  //   $filmWrapEl.append('<div class="film-frame"></div>' * filmRemain.currentFrame);
 });
 
 $rollCamBtn.on('click', () => {
   $filmCan1El.addClass('.spin');
   $filmCan2El.addClass('.spin');
+
+  filmRemain.filmDecrement();
+  $filmRemaning.html(filmRemain.currentFrame);
   console.log('Rolling!');
 });
